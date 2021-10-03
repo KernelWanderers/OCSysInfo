460fb0c14ce2c012f340980df835e11afb50e815
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-03 16:24:58 +0200

    new: DeviceManager instance – parent instance for retrieving a full list of hardware information

---
+++ devicemanager.py
@@ -0,0 +1,16 @@
+import platform
+
+"""
+TODO: Implement hardware dumps for Linux, Windows and macOS - and parse each dump accordingly.
+"""
+class DeviceManager:
+    """Instance responsible for exposing all important information about the current system's hardware."""
+    def __init__(self):
+        self.platform = platform.system().lower()
+
+        print(self.platform)
+
+    def dump(self):
+        print(self.platform)
+
+dm = DeviceManager()
