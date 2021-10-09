c83dd78aec44445d6c35faf3342b94b81abb57f0
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-09 19:00:42 +0200

    add: DeviceManager abstract class – parent placeholder for child instances

--- src/managers/devicemanager.py
+++ src/managers/devicemanager.py
@@ -6,11 +6,10 @@ TODO: Implement hardware dumps for Linux, Windows and macOS - and parse each dum
 class DeviceManager:
     """Instance responsible for exposing all important information about the current system's hardware."""
     def __init__(self):
+        self.info = {
+            "CPU": {},
+            "GPU": [],
+            "Network": [],
+            "Audio": {}
+        }
         self.platform = platform.system().lower()
-
-        print(self.platform)
-
-    def dump(self):
-        print(self.platform)
-
-dm = DeviceManager()
