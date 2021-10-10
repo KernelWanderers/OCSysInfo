7b6a4d85c008598b17af45073f3586375f07be50
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-10 04:48:47 +0200

    rename hardware managers

--- src/dumps/win_dump.py
+++
@@ -1,8 +0,0 @@
-from managers.devicemanager import DeviceManager
-
-class WinHardwareManager(DeviceManager):
-    def __init__(self):
-        self.cpu = ""
-
-    # def get_cpu_info(self):
-    #     model = 
