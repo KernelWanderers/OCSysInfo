0a31a96add4b70c4a867891bd82b72a461625c56
ObscureNightingale <twitchisadeck@gmail.com>
2021-10-09 23:47:27 -0700

    test

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 # from dumps.mac_dump import MacHardwareManager
 from managers.tree import tree
-from dumps.macOS.mac import MacHardwareManager
+from managers.devicemanager import DeviceManager
+# from dumps.macOS.mac import MacHardwareManager
 
 if __name__ == "__main__":
-    dump = MacHardwareManager()
-
-    dump.cpu_info()
-    dump.gpu_info()
-    dump.net_info()
-    dump.audio_info()
+    dump = DeviceManager()
 
     for key in dump.info:
         tree(key, dump.info[key])
