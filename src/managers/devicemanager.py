bfa8a60822ef0679551f59508234118a3dafe43a
ObscureNightingale <twitchisadeck@gmail.com>
2021-10-09 23:46:48 -0700

    refactor: add `pci` property, & dynamically set hardware managers

--- src/managers/devicemanager.py
+++ src/managers/devicemanager.py
@@ -1,8 +1,7 @@
 import platform
 
-"""
-TODO: Implement hardware dumps for Linux, Windows and macOS - and parse each dump accordingly.
-"""
+from managers.pciids import PCIIDs
+
 class DeviceManager:
     """Instance responsible for exposing all important information about the current system's hardware."""
     def __init__(self):
@@ -12,4 +11,14 @@ class DeviceManager:
             "Network": [],
             "Audio": []
         }
+        self.pci = PCIIDs()
         self.platform = platform.system().lower()
+
+        if self.platform == "darwin":
+            from dumps.macOS.mac import MacHardwareManager
+            self.manager = MacHardwareManager(self)
+        elif self.platform == "linux":
+            from dumps.Linux.linux import LinuxHardwareManager
+            self.manager = LinuxHardwareManager(self)
+
+        self.manager.dump()
