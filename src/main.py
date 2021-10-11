547a389e63285efc50cf04126de5a2531f97f5de
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-11 07:46:24 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-# from dumps.mac_dump import MacHardwareManager
 from managers.tree import tree
 from managers.devicemanager import DeviceManager
-# from dumps.macOS.mac import MacHardwareManager
 
 if __name__ == "__main__":
     dump = DeviceManager()
 
-    # for key in dump.info:
-    #     tree(key, dump.info[key])
-    #     print(" ")
+    for key in dump.info:
+        tree(key, dump.info[key])
+        print(" ")
