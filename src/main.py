c78e8e8b41ae0bb9321a983d23963cebf523b534
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-11 06:41:06 +0200

    test

--- src/main.py
+++ src/main.py
@@ -6,6 +6,6 @@ from managers.devicemanager import DeviceManager
 if __name__ == "__main__":
     dump = DeviceManager()
 
-    for key in dump.info:
-        tree(key, dump.info[key])
-        print(" ")
+    # for key in dump.info:
+    #     tree(key, dump.info[key])
+    #     print(" ")
