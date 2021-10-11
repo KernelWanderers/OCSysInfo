6df40e3de6547c3d6ee8ea8657a89612d6ee01ab
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-11 06:42:03 +0200

    refactor: implementation of scraping devicehunt.com - if nothing was found, try scraping pci-ids instead

--- src/managers/pciids.py
+++ src/managers/pciids.py
@@ -1,64 +1,54 @@
-import re
 import requests
-import aiohttp
 
 
 class PCIIDs:
     """
-    Abstraction for scraping the https://pci-ids.ucw.cz website.
+    Abstraction for scraping the https://devicehunt.com website.
+    If data is unavailable from the aforementioned website, it will attempt to scrape https://pci-ids.ucw.cz.
+
+    Thank you to @[CorpNewt](https://github.com/CorpNewt) for allowing us to copy over their own
+    implementation of scraping a website's response.
     """
 
-    async def get_item(self, deviceID: str, ven: str = "any") -> str:
+    def get_item(self, dev: str, ven: str = "any", types="pci") -> dict or None:
+        data = self.get_item_dh(dev, ven, types)
+
+        if not data and ven != "any":
+            data = self.get_item_pi(dev, ven)
+
+        return data or None
+
+    def get_item_dh(self, dev: str, ven: str = "any", types="pci") -> dict or None:
         content = requests.get(
-            "https://pci-ids.ucw.cz/read/PC/{}/{}".format(ven, deviceID.upper()))
+            "https://devicehunt.com/search/type/{}/vendor/{}/device/{}".format(types, ven.upper(), dev.upper()))
 
-        data = await self.async_dl(content.url)
+        if content.status_code != 200:
+            return None
 
-        if data:
-            out = ""
-            for line in data.decode().split('\n'):
-                if 'name' in line.lower() and not 'itemname' in line.lower():
-                    res = re.search(r'(?<=\<p\>)Name\:\s?.*(?=\<\/.+)', line)
-                    if res:
-                        out = res.group(0).split(':')[1].strip()
-                        break
-                elif 'itemname' in line.lower():
-                    out = "Name: ".join(line.split("Name: ")[1:]).replace("&amp;", "&").replace(
-                        "&quot;", '"').replace("&apos;", "'").replace("&gt;", ">").replace("&lt;", "<")
+        lines = content.text.split('\n')
+        device = {}
+        caught = None
 
-            if out:
-                return out
-            else:
-                return None
+        for line in lines:
+            cond = "--type-device" in line.lower()
+            if cond or ("--type-vendor" in line.lower() and caught):
+                device['device' if cond else "vendor"] = lines[lines.index(
+                    line.lower()) + 1].split('<')[0]
+                caught = device
 
-    async def async_post_text(self, url, data=None, headers=None):
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.post(url, data=data) as response:
-                res = await response.read()
-                return res.decode("utf-8", "replace")
+        return device or None
 
-    async def async_dl(self, url, headers=None):
-        total_size = 0
-        data = b""
-        async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(url) as response:
-                if response.status != 200:
-                    return None
+    def get_item_pi(self, dev: str, ven: str = "any") -> dict or None:
+        content = requests.get(
+            "https://pci-ids.ucw.cz/read/PC/{}/{}".format(ven, dev))
 
-                while True:
-                    chunk = await response.content.read(4*1024)  # 4k
-                    data += chunk
-                    total_size += len(chunk)
-                    if not chunk:
-                        break
-                    if total_size > 8000000:
-                        # Too big...
-                        return None
-        return data
+        if content.status_code != 200:
+            return None
 
-    async def async_text(self, url, headers=None):
-        data = await self.async_dl(url, headers)
-        if data != None:
-            return data.decode("utf-8", "replace")
-        else:
-            return data
+        device = ""
+
+        for line in content.text.split('\n'):
+            if 'itemname' in line.lower():
+                device = line.split('Name: ')[1]
+
+        return {'device': device} if device else None
