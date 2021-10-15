dda7f9a114a8b76b73944523c115e5bdeba45da6
ObscureNightingale <twitchisadeck@gmail.com>
2021-10-15 15:14:01 +0200

    test: dependency fix
    (hopefully) fix: dependency should install before the main script is loaded

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from cli.ui import UI
-from dependencies.dependency_manager import install_deps
-from managers.devicemanager import DeviceManager
-
 if __name__ == "__main__":
+    from dependencies.dependency_manager import install_deps
     install_deps()
 
+    from cli.ui import UI
+    from managers.devicemanager import DeviceManager
     dump = DeviceManager()
     ui = UI(dump)
 
