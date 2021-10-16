5ee1a31b166cb8e89aec3eea6277ee390cd9135c
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-16 09:21:17 +0200

    refactor: use `KeyboardInterrupt` exception to exit

--- src/main.py
+++ src/main.py
@@ -2,9 +2,16 @@ if __name__ == "__main__":
     from dependencies.dependency_manager import install_deps
     install_deps()
 
-    from cli.ui import UI
-    from managers.devicemanager import DeviceManager
-    dump = DeviceManager()
-    ui = UI(dump)
+    try:
+        from cli.ui import UI
+        from managers.devicemanager import DeviceManager
+    except:
+        install_deps()
 
-    ui.create_ui()
+    try: 
+        dump = DeviceManager()
+        ui = UI(dump)
+
+        ui.create_ui()
+    except KeyboardInterrupt:
+        exit(0)
