15b64274afb711cbf42214c72de64db72791da85
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-17 00:45:12 +0200

    refactor: ensure required dependencies are installed

--- src/main.py
+++ src/main.py
@@ -1,17 +1,34 @@
+#!/usr/bin/env python3
+
 if __name__ == "__main__":
-    from dependencies.dependency_manager import install_deps
-    install_deps()
+    import sys
+
+    if sys.version_info < (3, 6, 0):
+        print('OCSysInfo requires Python 3.6, while Python ' +
+              str(sys.version.partition(' ')[0] + ' was detected. Terminating. '))
+        sys.exit(1)
 
     try:
-        from cli.ui import UI
-        from managers.devicemanager import DeviceManager
-    except:
-        install_deps()
+        import dicttoxml
+        import requests
+        import distro
 
-    try: 
-        dump = DeviceManager()
-        ui = UI(dump)
+        if sys.platform.lower() == "darwin":
+            import objc
+    except Exception as e:
+        raise e
+        print("Please ensure you've installed the required dependencies.")
+    else:
+        try:
+            from cli.ui import UI
+            from managers.devicemanager import DeviceManager
+        except Exception as e:
+            raise e
+        else:
+            try:
+                dump = DeviceManager()
+                ui = UI(dump)
 
-        ui.create_ui()
-    except KeyboardInterrupt:
-        exit(0)
+                ui.create_ui()
+            except KeyboardInterrupt:
+                exit(0)
