35e7531cb0b38e9ebee7140c545fc8b704ac1ecc
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-16 09:20:55 +0200

    fix: dependency manager - disabled `shell`

--- src/dependencies/dependency_manager.py
+++ src/dependencies/dependency_manager.py
@@ -1,5 +1,4 @@
 import os
-import platform
 import subprocess
 import sys
 import pkg_resources
@@ -15,14 +14,31 @@ def install_deps():
         print(f"Attempting to install {missed}...")
         try:
             subprocess.call([sys.executable, '-m', 'pip',
-                            'install', missed, '--no-warn-script-location'], shell=True, stdout=open(os.devnull, "w"))
+                            'install', missed, '--no-warn-script-location'], stdout=open(os.devnull, "w"), shell=False)
             print(f"Successfully installed {missed}!")
         except Exception as e:
-            print(f"Error occurred. Unable to install {missed}")
+            path = os.path.join(os.path.dirname(
+                os.path.abspath(__file__)), "error_log.txt")
+            with open(path, "w") as file:
+                file.write(
+                    f"Failed to execute 'pip install -m {missed} --no-warn-script-location'\nError logs:\n\n{str(e)}")
+            print(
+                f"Error occurred. Unable to install {missed}\n\nDump is in: {path}")
 
-    if platform.platform().lower() == "darwin":
+    if sys.platform.lower() == "darwin":
         py_ver = str(sys.version_info.major) + \
             ('.' + str(sys.version_info.minor)
              if sys.version_info.minor != 0 else '')
-        subprocess.run(
-            '/Applications/Python\ {}/Install\ Certificates.command'.format(py_ver), shell=True)
+        try:
+            print("Attempting to install certificates for macOS...")
+            subprocess.call(
+                ['/Applications/Python {}/Install Certificates.command'.format(py_ver)], shell=False)
+            print("Successfully installed certificates!")
+        except Exception as e:
+            path = os.path.join(os.path.dirname(
+                os.path.abspath(__file__)), "macos_error_log.txt")
+            with open(path, "w") as file:
+                file.write(
+                    f"Failed to install certificates.\nError logs:\n\n{str(e)}")
+            print(
+                f"Error occurred. Unable to install certificates.\n\nDump is in: {path}")
