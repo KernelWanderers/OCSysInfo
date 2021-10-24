249389867bc90ebe2302cdcf7aee19a0a563ddfe
ObscureNightingale <twitchisadeck@gmail.com>
2021-10-24 17:20:30 +0200

    refactor: implement `stepping` logic

--- src/util/codename.py
+++ src/util/codename.py
@@ -1,12 +1,16 @@
-def codename(data, extf, family, extm, model):
+def codename(data, extf, family, extm, model, stepping=None):
     vals = []
     for arch in data:
 
         if extf.lower() == arch.get('ExtFamily', '').lower() and \
-           family.lower() == arch.get('BaseFamily', '').lower() and \
-           extm.lower() == arch.get('ExtModel', '').lower() and \
-           model.lower() == arch.get('BaseModel', '').lower():
+                family.lower() == arch.get('BaseFamily', '').lower() and \
+                extm.lower() == arch.get('ExtModel', '').lower() and \
+                model.lower() == arch.get('BaseModel', '').lower():
 
-            vals.append(arch.get('Codename'))
+            if stepping and arch.get('Stepping', None):
+                if stepping.lower() in arch.get('Stepping').lower():
+                    vals.append(arch.get('Codename'))
+            else:
+                vals.append(arch.get('Codename'))
 
     return vals
