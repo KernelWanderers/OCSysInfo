f6dcfc0a221ef90eeccb21e7010f85513cd4e2ee
ntwrkobscure <twitchisadeck@gmail.com>
2021-10-25 18:31:20 +0200

    implement: text formatting - Mahas1

--- src/info.py
+++ src/info.py
@@ -27,11 +27,21 @@ cyan = '\033[96m'
 green = '\033[92m'
 yellow = '\033[93m'
 red = '\033[91m'
-format_bold = '\033[1m'
-format_underline = '\033[4m'
+bold = '\033[1m'
+underline = '\033[4m'
 end_formatting = '\033[0m'
 
 
 def color_text(text, color):
     color_var = globals().get(color) if globals().get(color) else green
     return f"{color_var}{text}{end_formatting}"
+
+
+def format_text(text, formatting):
+    formatting_list = formatting.split("+")
+    final_string = text
+    for format in formatting_list:
+        format_var = globals().get(format) if globals().get(format) else None
+        if format_var:
+            final_string = f"{format_var}{final_string}{end_formatting}"
+    return final_string
