fc066613637f6d0781318f92d66f5ed2f63b5ea9
ObscureNightingale <twitchisadeck@gmail.com>
2021-10-27 19:42:04 +0200

    add: extract ACPI and PCI paths on Windows

--- src/util/pci_root.py
+++ src/util/pci_root.py
@@ -1,4 +1,7 @@
-def pci_from_acpi(raw_path):
+import re
+
+
+def pci_from_acpi_osx(raw_path):
     if not raw_path:
         return
 
@@ -25,3 +28,62 @@ def pci_from_acpi(raw_path):
             f_path += f'/Pci({n},{k})'
 
     return f_path
+
+
+def pci_from_acpi_win(wmi, name):
+
+    try:
+        raw_path = wmi.query(f'SELECT * FROM Win32_PnPEntity WHERE Name=\'{name}\'')[
+            0].GetDeviceProperties(['DEVPKEY_Device_LocationPaths'])[0][0].Data
+    except:
+        return {}
+
+    if not raw_path:
+        return
+
+    data = {
+        'PCI Path': '',
+        'ACPI Path': ''
+    }
+
+    pci, acpi = raw_path
+
+    if pci != '#':
+        path = ''
+
+        for arg in pci.split('#'):
+            digit = re.search(r'(?<=\()(\d*|\w*)+(?=\))', arg)
+
+            if not digit:
+                path = None
+                break
+
+            digit = digit.group()
+
+            if 'pciroot' in arg.lower():
+                path += f'PciRoot({hex(int(digit, 16))})'
+                continue
+
+            path += f'/Pci({hex(int(digit[0:2], 16))},{hex(int(digit[2:], 16))})'
+
+        data['PCI Path'] = path
+
+    if acpi != '#':
+        path = ''
+
+        for arg in acpi.split('#'):
+            if '_SB' in arg:
+                path += '\_SB'
+                continue
+
+            prop = re.search(r'(?<=\()(\w*|\d*)+(?=\))', arg)
+
+            if not prop:
+                path = None
+                break
+
+            path += f'.{prop.group()}'
+
+        data['ACPI Path'] = path
+
+    return data
