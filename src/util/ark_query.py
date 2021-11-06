202ffad006ab98a5fda320b56aabe3d0ab15af21
ntwrkobscure <twitchisadeck@gmail.com>
2021-11-06 11:56:16 +0100

    new: query for intel's ARK page

--- src/util/ark_query.py
+++ src/util/ark_query.py
@@ -1,31 +1,52 @@
-import requests
+# Copyright 2016 Major Hayden
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
+import requests
 import xmltodict
 
 
 def get_full_ark_url(prod_url):
-    # we get the product URL from the quick search results,
-    # and then append it to the base URL
+    """
+    Method to obtain the product URL from the quick search results,
+    and append it to the base URL.
+    """
     full_url = "https://ark.intel.com{0}".format(prod_url)
     return full_url
 
 
 def quick_search(search_term):
-    # credits to https://github.com/xiongnemo/arksearch (a fork of major/arksearch) for this code
-    url = "https://ark.intel.com/libs/apps/intel/arksearch/autocomplete?" + \
-          "_charset_=UTF-8" + \
-          "&locale=en_us" + \
-          "&currentPageUrl=https%3A%2F%2Fark.intel.com%2Fcontent%2Fwww%2Fus%2Fen%2Fark.html" + \
-          "&input_query={0}"
+    # Credits to https://github.com/xiongnemo/arksearch (a fork of major/arksearch) for this.
+    url = (
+        "https://ark.intel.com/libs/apps/intel/arksearch/autocomplete?"
+        + "_charset_=UTF-8"
+        + "&locale=en_us"
+        + "&currentPageUrl=https%3A%2F%2Fark.intel.com%2Fcontent%2Fwww%2Fus%2Fen%2Fark.html"
+        + "&input_query={0}"
+    )
 
     r = requests.get(url.format(search_term))
     return r.json()
 
 
 def get_codename(ark_url):
-    # we get the Ark URL from the quick search results,
-    # and get the HTML data from the URL.
-    # Parsing it, and then getting the codename from the HTML
+    """
+    We get the ARK URL from the quick search results,
+    and obtain the HTML data.
+
+    Parsing it, we can get the codename.
+    """
     text_thing = requests.get(ark_url).content.decode("utf8")
     lines = text_thing.split("\n")
     actual_line = {"a": {}}
@@ -33,8 +54,12 @@ def get_codename(ark_url):
         if 'data-key="CodeNameText"' in lines[line_index]:
             actual_line = lines[line_index + 1].strip()
 
-    line_json = xmltodict.parse(actual_line)  # we only need to parse the one line
-    codename = line_json.get("a").get("#text")  # The codename is wrapped in an <a> tag
+    # We only need to parse the one line.
+    line_json = xmltodict.parse(actual_line)
+
+    # The codename is wrapped in an <a> tag.
+    codename = line_json.get("a").get("#text")
+
     return codename if codename else None
 
 
@@ -50,7 +75,7 @@ def simplified_name(cpu_name):
         "(C)": "©",
         "(P)": "℗",
         "(G)": "℠",
-        "CPU": ""
+        "CPU": "",
     }
     result_name = cpu_name.split("@")[0].strip()
     for key, value in replace_dict.items():
