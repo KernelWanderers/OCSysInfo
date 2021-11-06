6b58348be2e78d1c5117522f85811c018a8165b7
ntwrkobscure <twitchisadeck@gmail.com>
2021-11-06 11:58:02 +0100

    new: query `wikichip` for AMD's codenames

---
+++ src/util/wc_amd_query.py
@@ -0,0 +1,76 @@
+import requests
+import re
+
+BASE_URL = "https://en.wikichip.org/wiki/amd"
+
+
+def parse_codename(cpu_name):
+    """
+    Queries the AMD section from wikichip,
+    parses the contents, and looks for the codename/µarch.
+    """
+
+    formatted = re.sub(
+        r"(\d{1,2}?(-Core\s?)?(Processor))", "", cpu_name.replace("AMD", "")
+    ).strip()
+
+    family = ""
+    model = ""
+
+    # Format data for request properly
+    if "ryzen" in formatted.lower():
+        family = "_".join(formatted.split(" ")[:2]).lower()
+        model = formatted.split(" ")[2].lower()
+    else:
+        model = formatted.lower()
+
+    URL = ""
+
+    if family:
+        URL = f"{BASE_URL}/{family}/{model}"
+    else:
+        URL = f"{BASE_URL}/{model}"
+
+    URL = URL.replace(" ", "_")
+
+    contents = requests.get(URL).content.decode("utf-8")
+    data = {"Microarchitecture": "", "Codename": ""}
+
+    try:
+        data["Codename"] = re.search(
+            r"(?<=\>).+(?=\<)",
+            re.search(
+                r"\<a\s?href=\"\/wiki\/amd\/cores\/(\w|\d)+\s?(\w|\d)+?\"[^>]+\>[^<]+?\<\/a\>",
+                contents,
+            ).group(),
+        ).group()
+    except Exception:
+        pass
+
+    try:
+        data["Microarchitecture"] = re.search(
+            r"(?<=\>).+(?=\<)",
+            re.search(
+                r"\<a\s?href=\"\/wiki\/amd\/microarchitectures\/(\w|\d)+\s?(\w|\d)+?\"[^>]+\>[^<]+?\<\/a\>",
+                contents,
+            ).group(),
+        ).group()
+    except Exception:
+        pass
+
+    try:
+        if not data["Microarchitecture"]:
+            data["Microarchitecture"] = re.search(
+                r"(?<=\>).+(?=\<)",
+                re.search(
+                    r"\<a\s?href=\"\/wiki\/amd\/(\w|\d)+\s?(\w|\d)+?\s?\(microarch\)\"[^>]+\>[^<]+?\<\/a\>",
+                    contents,
+                ).group(),
+            ).group()
+    except Exception:
+        pass
+
+    if not data["Microarchitecture"] and not data["Codename"]:
+        return None
+
+    return data
