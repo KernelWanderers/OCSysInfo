c7246a16ee2b26b5a457337a15231e8326b9141c
ObscureNightingale <twitchisadeck@gmail.com>
2021-11-09 21:56:21 +0100

    add(win_enum): memory types

--- src/dumps/Windows/win_enum.py
+++ src/dumps/Windows/win_enum.py
@@ -27,6 +27,35 @@ MEDIA_TYPE = {
     5: "Storage Class Memory (SCM)",
 }
 
+MEMORY_TYPE = {
+    0: "Unknown",
+    1: "Other",
+    2: "DRAM",
+    3: "Synchronous DRAM",
+    4: "Cache DRAM",
+    5: "EDO",
+    6: "EDRAM",
+    7: "VRAM",
+    8: "SRAM",
+    9: "RAM",
+    10: "ROM",
+    11: "Flash",
+    12: "EEPROM",
+    13: "FEPROM",
+    14: "EPROM",
+    15: "CDRAM",
+    16: "3DRAM",
+    17: "SDRAM",
+    18: "SGRAM",
+    19: "RDRAM",
+    20: "DDR",
+    21: "DDR2",
+    22: "DDR2 FB-DIMM",
+    24: "DDR3",
+    25: "FBD2",
+    26: "DDR4"
+}
+
 POINT_DEV_INTERFACE = {
     1: "Other",
     2: "Unknown",
