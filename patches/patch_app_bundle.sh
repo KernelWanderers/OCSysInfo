db435b08ccaefc8840e186fcc19288383b0cd62e
kernelBPD <twitchisadeck@gmail.com>
2022-05-20 21:12:24 +0200

    hopefully fix

---
+++ patches/patch_app_bundle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Special thank you to [khronokernel](https://github.com/khronokernel)
+# for this.
+#
+# Source: https://github.com/dortania/OpenCore-Legacy-Patcher/blob/main/payloads/launcher.sh
+cd "$(dirname "$0")"
+
+chmod +x OCSysInfo
+open OCSysInfo
