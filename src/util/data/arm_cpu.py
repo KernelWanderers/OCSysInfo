362e5bc371f1efc09fae6c28e307409ad0ac4d81
kernel-dev <twitchisadeck@gmail.com>
2022-07-13 07:32:37 +0200

    add: Apple ARM64 codename support

---
+++ src/util/data/arm_cpu.py
@@ -0,0 +1,41 @@
+ARM_CPU = {
+    "M2": {
+        "arm_version": "ARMv8.5-A",
+        "performance_core": "Avalanche",
+        "efficiency_core": "Blizzard",
+        "codename": "",
+        "part_no": ""
+    },
+
+    "M1 Ultra": {
+        "arm_version": "ARMv8.5-A",
+        "performance_core": "Firestorm",
+        "efficiency_core": "Icestorm",
+        "codename": "APL1W06",
+        "part_no": "T6002"
+    },
+
+    "M1 Max": {
+        "arm_version": "ARMv8.5-A",
+        "performance_core": "Firestorm",
+        "efficiency_core": "Icestorm",
+        "codename": "APL1105",
+        "part_no": "T6001"
+    },
+
+    "M1 Pro": {
+        "arm_version": "ARMv8.5-A",
+        "performance_core": "Firestorm",
+        "efficiency_core": "Icestorm",
+        "codename": "APL1103",
+        "part_no": "T6000"
+    },
+
+    "M1": {
+        "arm_version": "ARMv8.5-A",
+        "performance_core": "Firestorm",
+        "efficiency_core": "Icestorm",
+        "codename": "APL1102",
+        "part_no": "T8103"
+    }
+}
