86f243a91e8bdba779810f6877b9dca0c53ced03
Mahas1 <mahasvan@gmail.com>
2023-11-10 13:13:53 +0530

    add localization to patch_plist.py

--- patches/patch_plist.py
+++ patches/patch_plist.py
@@ -1,21 +1,27 @@
+import os
 import plistlib
 from pathlib import Path
 
 
+from src.info import localizations, project_root
+from localization.langparser import LangParser
+langparser = LangParser(localizations, project_root, os.environ.get("LANGUAGE", "English"))
+
+
 def patch_info_plist():
-    print("Updating Info.plist...")
+    print(langparser.parse_message("updating_info_plist"))
     plist_path = "./dist/OCSysInfo.app/Contents/Info.plist"
 
-    print("Patching CFBundleExecutable...")
+    print(langparser.parse_message("patching_cfbundleexecutable"))
     plist = plistlib.load(Path(plist_path).open("rb"))
 
     plist["CFBundleExecutable"] = "Launcher"
+    print(langparser.parse_message("writing_changes"))
 
-    print("Writing changes...")
     plistlib.dump(plist, Path(
         plist_path).open("wb"), sort_keys=True)
 
-    print("Successfully patched Info.plist!")
+    print(langparser.parse_message("successfully_patched_info_plist"))
 
 
 patch_info_plist()
